f9351de60035770aaaa31210464e87776cc4ae0c
fahmi ahmad <fahmiahmad@tutanota.com>
2026-06-17 22:26:03 +0700

    struct stats

--- host/sysmon/sysmon/main.swift
+++ host/sysmon/sysmon/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+struct StatsSample {
+    let cpu: Int
+    let memory: Int
+    let temperature: Int
+    let rx: Int
+    let tx: Int
+    
+    func protocolLine() -> String {
+        "CPU:\(cpu),MEM:\(memory),TMP:\(temperature),RX:\(rx),TX:\(tx)"
+    }
+}
 
+let sample = StatsSample(cpu: 42, memory: 67, temperature: 58, rx: 1234, tx: 88)
+print(sample.protocolLine())
